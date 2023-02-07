99f1690f16421e29694f218efb145dc1102bcf6b
Soren Marcelino <soren.marcelino@airbus.com>
2023-02-07 11:50:34 +0100

    Initial Commit

---
+++ TestUI/TestUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestUIApp.swift
+//  TestUI
+//
+//  Created by Soren Marcelino on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct TestUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
