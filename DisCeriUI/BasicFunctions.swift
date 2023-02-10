1e9315ce2bf6161605842f58d39c89c54a26e53e
Soren Marcelino <soren.marcelino@icloud.com>
2023-02-10 14:57:25 +0100

    iPod UI DONE + Siri button IN PROGRESS

---
+++ DisCeriUI/BasicFunctions.swift
@@ -0,0 +1,15 @@
+//
+//  BasicFunctions.swift
+//  IpodSwiftUI
+//
+//  Created by Soren Marcelino on 09/02/2023.
+//
+
+import Foundation
+
+class BasicFunctions {
+    
+    public func debugTest() -> String {
+        return "Ceci est un test de degbug"
+    }
+}
--- DisCeriUI/DisCeriUIApp.swift
+++ DisCeriUI/DisCeriUIApp.swift
@@ -1,14 +1,14 @@
 //
-//  TestUIApp.swift
-//  TestUI
+//  IpodSwiftUIApp.swift
+//  IpodSwiftUI
 //
-//  Created by Soren Marcelino on 07/02/2023.
+//  Created by Shreyas Vilaschandra Bhike on 05/12/21.
 //
 
 import SwiftUI
 
 @main
-struct TestUIApp: App {
+struct DisCeriUIApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
