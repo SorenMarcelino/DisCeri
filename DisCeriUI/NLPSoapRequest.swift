1d264f993b38591a21ea8126e7638818f47d9203
Theo QUEZEL PERRON <explo8413@gmail.com>
2023-04-02 17:43:22 +0200

    Add nlp soap

--- DisCeriUI/NLPSoapRequest.swift
+++ DisCeriUI/NLPSoapRequest.swift
@@ -8,29 +8,28 @@
 import Foundation
 import Swifter
 
-class ASRSoapRequest {
-    func requestASR(audioURL: URL) {
+class NLPSoapRequest {
+    func requestNLP(text: String) {
         /*guard let url = Bundle.main.url(forResource: "MacronASRTestPart", withExtension: "mp3") else {
             print("Error: Failed to find the music file.")
             return
         }*/
         
         // Read the audio file as data
-        guard let audioData = try? Data(contentsOf: audioURL) else {
-            print("Could not read audio file")
+        guard let text = try? Data(contentsOf: text) else {
+            print("Could not read str")
             return
         }
         
         // Encode the audio file as base64
-        let base64Audio = audioData.base64EncodedString()
-        print("Audio : \(base64Audio)")
+        let str = text
         
         // Define the SOAP request payload with the base64-encoded audio
         let soapMessage = """
         <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
             <soap:Body>
                 <transcriptionRequest>
-                    <audio>\(base64Audio)</audio>
+                    <text>\(str)</text>
                 </transcriptionRequest>
             </soap:Body>
         </soap:Envelope>
@@ -40,11 +39,11 @@ class ASRSoapRequest {
         let ipCERI = "10.126.2.87"
         let ipSoren = "192.168.1.154"
         let ipTheo = "192.168.1.12"
-        let urlString = "http://\(ipTheo):45876/transcribe"
+        let urlString = "http://\(ipTheo):45877/"
         
         // Create a URL request with the SOAP message as the body
         var request = URLRequest(url: URL(string: urlString)!)
-        request.httpMethod = "POST"
+        request.httpMethod = "GET"
         request.addValue("text/xml", forHTTPHeaderField: "Content-Type")
         request.httpBody = soapMessage.data(using: .utf8)
         
@@ -62,7 +61,6 @@ class ASRSoapRequest {
             
             print("Response status code: \(response.statusCode)")
             print("Response data: \(String(data: data, encoding: .utf8) ?? "")")
-            requestNLP(text: data)
         }
         task.resume()
     }
